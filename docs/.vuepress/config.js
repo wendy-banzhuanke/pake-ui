b270f7ae3570c0fd20b0828d34358205aa39d3f5
zhangjian <13522225365@163.com>
2020-08-12 10:50:51 +0800

    增加侧边栏sidebar配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,29 @@
 module.exports = {
-  title: '南宫的博客',
+  title: 'PAKE UI',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
-    ['link', { rel: 'icon', href: '/avatar.jpeg' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ['link', { rel: 'icon', href: '/avatar01.jpeg' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
   themeConfig: {
-    logo: '/avatar.jpeg',  // 左上角logo
+    logo: '/avatar01.jpeg',  // 左上角logo
     nav:[ // 导航栏配置
       {text: '首页', link: '/' },
       {text: '技术文档', link: '/tech/' },
-      {text: '简书主页', link: 'https://www.jianshu.com/u/c455567c7f50'}      
+      {text: '个人主页', link: 'https://www.banzhuanke.com'}      
     ],
-    sidebar: 'auto', // 侧边栏配置
+    // sidebar: 'auto', // 侧边栏配置
+    sidebar: [
+      '',
+      {
+        title: '美丽的css',
+        path: '/tech/',
+        collapsable: false, //是否展开
+        sidebarDepth: 0,
+        children: [
+          '/tech/css01',
+          '/tech/css02',
+        ],
+      }
+    ],
+    sidebarDepth: 2,
   }
 };
--- docs/tech/README.md
+++ docs/tech/README.md
@@ -1,1 +1,9 @@
+---
+navbar: true
+title: tech-one
+---
+
+热爱生活
+
+### 标题三级
 <loading/>
